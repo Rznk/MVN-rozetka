4415e994b3e27865bffe12d89afe7b495cf0da4c
Andrew Reznik <rezyadlf@gmail.com>
2017-03-29 15:13:28 +0300

    Errors fixed

---
+++ src/main/java/Rozetka/Config.java
@@ -0,0 +1,16 @@
+package Rozetka;
+
+/**
+ * Created by Andrew on 3/29/2017.
+ */
+public class Config {
+
+    public static final String MAIN_PAGE_URL = "http://rozetka.com.ua";
+
+    public static void setUpBrowser() {
+        System.setProperty("webdriver.chrome.driver", "src/main/java/driver/geckodriver.exe");
+    }
+
+    private GlobalConfig() {
+    }
+}
